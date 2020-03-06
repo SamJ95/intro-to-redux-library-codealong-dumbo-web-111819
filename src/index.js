a26405fb4cf98f467032b44d6ca2407e55dad3a2
Samir Jasarat <samirjasarat@gmail.com>
2020-03-06 14:06:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer.js';
 import App from './App';
+import {Provider} from 'react-redux';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+//createStore method is passed a reducer 
+
+const store = createStore(shoppingListItemReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+    );
+
+//Store is passed to the App component as a prop 
+ReactDOM.render(
+    <Provider store ={store}>
+    <App />
+    </Provider>
+    , document.getElementById('root'));
+
+//Provider allows for the access to the Redux store or 
+//dispatch actions from any component
+
+//Store is a creation that utilizes the createStore function and
+//a reducer function
+
+//Connect allows the store to map its data to the rest of the
+//Redux components 
+
+//Every button click dispatches an action to the store, causing it to change.
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -4,13 +4,20 @@ export default function shoppingListItemReducer(
 	},
 	action
 ) {
+	//A reducer function which basically uses switch and case
+	//statements to update state through an action. The reducer is producing a 
+	//a counter 
+
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length);
+			console.log('Updating state.items length to s%', state.items.length +1);
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('Initial state.items length: %s', state.items.length);
 			return state;
 	}
 }
